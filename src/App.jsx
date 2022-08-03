3d3b52d25cd731b388e3007c3b2ac1c61605954c
ademclk <ademonurcelik@icloud.com>
2022-08-03 10:37:01 +0300

    Frontend for search.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    const lastquery = localStorage.getItem("lastquery");
+    search(lastquery); 
+  }, []);
+
+  const search = async (q) => {
+    const response = await fetch('http://localhost:8080?' + new URLSearchParams({ q }));
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem("lastquery", q);
+  };
 
   return (
-    <div className="App">
-    
-    </div>
-  )
+    <main>
+      <h1>Animal Farm</h1>
+
+      <input
+        type="text"
+        placeholder="Enter a keyword"
+        onChange={(e) => search(e.target.value)}
+      />
+
+      <ul>
+        {animals.map((animal) => (
+          <li key={animal.id}>
+            <strong>{animal.type} </strong>
+            {animal.name} {animal.age}
+          </li>
+        ))}
+        {animals.length === 0 && <p>No animals found</p>}
+      </ul>
+    </main>
+  );
 }
 
-export default App
+export default App;
